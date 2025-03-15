18ff7b81af738b01e5a99599af709306b0463c43
Azeez muhammed <muhammedazeez09@gmail.com>
2025-03-15 09:34:30 +0100

    error fixed.

--- model/userModel.js
+++ model/userModel.js
@@ -1,60 +1,64 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  first_lastName: {
-    type: String,
-    required: true,
-  },
-  other_names: {
-    type: String,
-    required: false,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  parent_guardian_name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 255,
-  },
-  confirmPassword: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 255,
-    validate: function (value) {
-      return this.password === value;
+const userSchema = new mongoose.Schema(
+  {
+    first_lastName: {
+      type: String,
+      required: true,
     },
-    message: "Check Password",
+    other_names: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    parent_guardian_name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      // minlength: 8,
+      // maxlength: 255,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+      // minlength: 8,
+      // maxlength: 255,
+      validate: function (value) {
+        return this.password === value;
+      },
+      message: "Check Password",
+    },
+    forgotPasswordOTP: {
+      type: String,
+      default: "",
+    },
+    forgotPasswordOTPExpireAt: {
+      type: Date,
+      default: Date.now,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    cloudinary_id: {
+      type: String,
+    },
+    roles: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  forgotPasswordOTP: {
-    type: String,
-    default: "",
-  },
-  forgotPasswordOTPExpireAt: {
-    type: Date,
-    default: Date.now,
-  },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-  cloudinary_id: {
-    type: String,
-  },
-  roles: {
-    enum: ["admin", "user"],
-    default: "user",
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
