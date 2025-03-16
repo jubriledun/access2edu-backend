8ba86202e896e2275b5913a6ca6417b73aeef513
Azeez muhammed <muhammedazeez09@gmail.com>
2025-03-16 17:43:00 +0100

    ok

--- server.js
+++ server.js
@@ -4,15 +4,9 @@ import cookieParser from "cookie-parser";
 import globalError from "./config/globalErrors.js";
 import logger from "./config/logger.js";
 import connectDB from "./config/db.js";
-<<<<<<< HEAD
 import studentRouter from "./routes/studentRouter.js";
 import subjectRouter from "./routes/subjectRouter.js";
 import adminRouter from "./routes/adminRouter.js";
-=======
-import userRouter from "./routes/userRouters.js";
-import paymentRoutes from './routes/paymentRoutes.js';
-import webhookRoutes from './routes/webhookRoutes.js';
->>>>>>> a52ef16b30fa8cc55f41f6f3086d2f39c296843e
 
 const app = express();
 
@@ -25,17 +19,9 @@ app.use(
   })
 );
 
-<<<<<<< HEAD
 app.use("/api/v1/students", studentRouter);
 app.use("/api/v1/subjects", subjectRouter);
 app.use("/api/v1/admin", adminRouter);
-=======
-app.use("/api/v1/users", userRouter);
-app.use("/api/payments", paymentRoutes);
-app.use("/webhooks", webhookRoutes);
-
-app.get("/", (req, res) => res.send("Payment Gateway API Running"));
->>>>>>> a52ef16b30fa8cc55f41f6f3086d2f39c296843e
 
 app.all("*", (req, res, next) => {
   res.status(400).json({
